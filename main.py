4ddf51afd62ef885ee3d3b7e1b5b2f69a2d1a74c
cachola25 <cachola25@up.edu>
2024-04-13 14:07:48 -0700

    Camera Stuff

--- main.py
+++ main.py
@@ -2,49 +2,49 @@ import numpy as np
 import nanocamera as nano
 import cv2
 
-cams_test = 10
-for i in range(0, cams_test):
-    vid = cv2.VideoCapture(i)
-    test, frame = vid.read()
-    print("i : "+str(i)+" /// result: "+str(test))
-    while(True): 
+# cams_test = 10
+# for i in range(0, cams_test):
+#     vid = cv2.VideoCapture(i)
+#     test, frame = vid.read()
+#     print("i : "+str(i)+" /// result: "+str(test))
+#     while(True): 
         
-        # Capture the video frame 
-        # by frame 
-        ret, frame = vid.read() 
+#         # Capture the video frame 
+#         # by frame 
+#         ret, frame = vid.read() 
     
-        # Display the resulting frame 
-        cv2.imshow('frame', frame) 
+#         # Display the resulting frame 
+#         cv2.imshow('frame', frame) 
         
-        # the 'q' button is set as the 
-        # quitting button you may use any 
-        # desired button of your choice 
-        if cv2.waitKey(1) & 0xFF == ord('q'): 
-            break
+#         # the 'q' button is set as the 
+#         # quitting button you may use any 
+#         # desired button of your choice 
+#         if cv2.waitKey(1) & 0xFF == ord('q'): 
+#             break
     
-    # After the loop release the cap object 
-    vid.release() 
-    # Destroy all the windows 
-    cv2.destroyAllWindows() 
+#     # After the loop release the cap object 
+#     vid.release() 
+#     # Destroy all the windows 
+#     cv2.destroyAllWindows() 
 
-# vid = cv2.VideoCapture(2) 
+vid = cv2.VideoCapture(0) 
   
-# while(True): 
+while(True): 
       
-#     # Capture the video frame 
-#     # by frame 
-#     ret, frame = vid.read() 
+    # Capture the video frame 
+    # by frame 
+    ret, frame = vid.read() 
   
-#     # Display the resulting frame 
-#     cv2.imshow('frame', frame) 
+    # Display the resulting frame 
+    cv2.imshow('frame', frame) 
       
-#     # the 'q' button is set as the 
-#     # quitting button you may use any 
-#     # desired button of your choice 
-#     if cv2.waitKey(1) & 0xFF == ord('q'): 
-#         break
+    # the 'q' button is set as the 
+    # quitting button you may use any 
+    # desired button of your choice 
+    if cv2.waitKey(1) & 0xFF == ord('q'): 
+        break
   
-# # After the loop release the cap object 
-# vid.release() 
-# # Destroy all the windows 
-# cv2.destroyAllWindows() 
+# After the loop release the cap object 
+vid.release() 
+# Destroy all the windows 
+cv2.destroyAllWindows() 
