021fe3371efe8b808c214a9c44c61a4fb8e00517
cachola25 <cachola25@up.edu>
2024-04-16 08:49:48 -0700

    Committing dataset changes

--- create_lmdb/create_data.sh
+++ create_lmdb/create_data.sh
@@ -31,6 +31,6 @@ then
 fi
 for subset in test trainval
 do
-  python3 /home/vkchcp0013/Documents/mobile-ssd/caffe/scripts/create_annoset.py --anno-type=$anno_type --label-type=$label_type --label-map-file=$mapfile --min-dim=$min_dim --max-dim=$max_dim --resize-width=$width --resize-height=$height --check-label $extra_cmd $data_root_dir $root_dir/$subset.txt $data_root_dir/$dataset_name/$db/$dataset_name"_"$subset"_"$db examples/$dataset_name
+  python3 /Users/jayvencachola/Desktop/caffe/scripts/create_annoset.py --anno-type=$anno_type --label-type=$label_type --label-map-file=$mapfile --min-dim=$min_dim --max-dim=$max_dim --resize-width=$width --resize-height=$height --check-label $extra_cmd $data_root_dir $root_dir/$subset.txt $data_root_dir/$dataset_name/$db/$dataset_name"_"$subset"_"$db examples/$dataset_name
 done
 
