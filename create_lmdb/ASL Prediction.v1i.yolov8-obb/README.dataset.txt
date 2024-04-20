48bf9351bdfbd8478fa63986bac4161fde629258
cachola25 <cachola25@up.edu>
2024-04-20 15:26:46 -0700

    Image changes

--- create_lmdb/ASL Prediction.v1i.yolov8-obb/README.dataset.txt
+++
@@ -1,6 +0,0 @@
-# ASL Prediction > 2023-10-01 10:22am
-https://universe.roboflow.com/pasha-khoshkebari-ypvgn/asl-prediction
-
-Provided by a Roboflow user
-License: CC BY 4.0
-
--- create_lmdb/ultra.py
+++ create_lmdb/ultra.py
@@ -1,1 +1,6 @@
 from ultralytics import YOLO
+
+model = YOLO('yolov8n.yaml')
+model.train(data='./dataset/data.yaml',epochs=3)
+metrics = model.val()
+path = model.export(format='onnx')
