6d94bc3fe631246f7b44c97f44eac7ec53ad25f1
cachola25 <cachola25@up.edu>
2024-04-20 15:37:13 -0700

    New model-1 epoch

--- create_lmdb/ultra.py
+++ create_lmdb/ultra.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
 model = YOLO('yolov8n.yaml')
-model.train(data='./dataset/data.yaml',epochs=3)
+model.train(data='./dataset/data.yaml',epochs=1)
 metrics = model.val()
 path = model.export(format='onnx')
